2fcd46db5137e009abdb46a81e8130779226793e
Havret <h4vret@gmail.com>
2019-10-27 14:59:54 +0100

    Sending enums with flags using grpc - example

---
+++ EnumExampleClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using EnumExample;
+using Grpc.Core;
+
+namespace EnumExampleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+
+            var client = new DeploymentCalendar.DeploymentCalendarClient(channel);
+
+            var reply = client.GetDeploymentDays(new DeploymentDaysRequest() { NumberOfDeployments = 3 });
+            var deploymentDays = (Weekend) reply.DeploymentDays;
+
+            Console.WriteLine("Deployment will be in:");
+            if ((deploymentDays & Weekend.Friday) == Weekend.Friday || (deploymentDays & Weekend.Saturday) == Weekend.Saturday || (deploymentDays & Weekend.Sunday) == Weekend.Sunday)
+            {
+                Console.WriteLine("There will be deployment during the weekend. :(");                
+            }
+            else
+            {
+                Console.WriteLine("There won't be any deployment over the weekend.");
+            }
+
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EnumExampleServer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using EnumExample;
+using EnumExampleClient;
+using Grpc.Core;
+
+namespace EnumExampleServer
+{
+    class GreeterImpl : DeploymentCalendar.DeploymentCalendarBase
+    {
+        public override Task<DeploymentDaysReply> GetDeploymentDays(DeploymentDaysRequest request, ServerCallContext context)
+        {
+            Weekend deploymentDays = Weekend.Friday | Weekend.Saturday | Weekend.Sunday;
+            return Task.FromResult(new DeploymentDaysReply
+            {
+                DeploymentDays = (int) deploymentDays
+            });
+        }
+    }
+
+    class Program
+    {
+        const int Port = 50051;
+
+        public static void Main(string[] args)
+        {
+            Server server = new Server
+            {
+                Services = { DeploymentCalendar.BindService(new GreeterImpl()) },
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("Deployment calendar server listening on port " + Port);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+}
